0d69b600a78a6df0b6a8a20c9c60ccf96d35e5df
Alif Mustaqim <alifmus12@gmail.com>
2020-10-06 11:16:30 +0700

    Fix not showing Main Act and Add some changes

--- app/src/main/java/com/example/android/populartvapp/SplashscreenActivity.java
+++ app/src/main/java/com/example/android/populartvapp/SplashscreenActivity.java
@@ -3,26 +3,28 @@ package com.example.android.populartvapp;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
+import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashscreenActivity extends AppCompatActivity {
 
-    private int waktu_loading=2000; //2000 = 2 detik
+    private int waktu_loading = 2000; //2000 = 2 detik (Waktu SplashScreen)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splashscreen);
-        new Handler().postDelayed(new Runnable() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
                 //setelah loading maka akan langsung berpindah ke home activity
-                Intent IntroActivity=new Intent(SplashscreenActivity.this, IntroActivity.class);
-                startActivity(IntroActivity);
+                Intent intent = new Intent(SplashscreenActivity.this, MainActivity.class);
+                startActivity(intent);
                 finish();
             }
         },waktu_loading);
+
     }
 }
