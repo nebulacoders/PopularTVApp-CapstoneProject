6f15580e94ec5e692ec183672396bdbf9781f587
gilbert378 <gilbertfrans16@gmail.com>
2020-11-29 01:13:54 +0700

    .

---
+++ app/src/main/java/com/example/android/populartvapp/AlarmReceiver.java
@@ -0,0 +1,38 @@
+package com.example.android.populartvapp;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 1012;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        deliverNotification(context);
+    }
+
+    private void deliverNotification(Context context) {
+        Intent contentIntent = new Intent(context, SplashscreenActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.logonebula)
+                .setContentTitle("Search New Popular TV Shows")
+                .setContentText("Check out new popular tv series now!")
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+}
+
--- app/src/main/java/com/example/android/populartvapp/MainActivity.java
+++ app/src/main/java/com/example/android/populartvapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.populartvapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -11,14 +12,24 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
@@ -41,7 +52,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MainActivity";
     public SharedPreferences prefs;
+    public SharedPreferences.Editor editor;
     public boolean firstStart;
 
     private Toolbar toolbar;
@@ -54,6 +67,12 @@ public class MainActivity extends AppCompatActivity {
 
     private TVSeriesViewModel mTVSeriesViewModel;
 
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 1012;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private AlarmManager alarmManager;
+    private PendingIntent notifyPendingIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,11 +80,15 @@ public class MainActivity extends AppCompatActivity {
 
         //SharedPrefences IntroActivity
         prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        editor = prefs.edit();
         firstStart = prefs.getBoolean("firstStart", true);
         if (firstStart) {
             Intent intentIntro = new Intent(this, IntroActivity.class);
             startActivity(intentIntro);
-        }
+
+            enableNotification(null);
+        } else
+            recordRunTime();
 
         //initView
         toolbar = findViewById(R.id.toolbar);
@@ -91,6 +114,13 @@ public class MainActivity extends AppCompatActivity {
         viewPagerAdapter.addFragment(secondFragment, "Search");
         viewPager.setAdapter(viewPagerAdapter);
 
+        Intent notifyIntent = new Intent(this, AlarmReceiver.class);
+        notifyPendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        createNotificationChannel();
+
     }
 
     private class ViewPagerAdapter extends FragmentPagerAdapter {
@@ -102,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
             super(fm, behavior);
         }
 
-        public void addFragment(Fragment fragment, String title){
+        public void addFragment(Fragment fragment, String title) {
             fragments.add(fragment);
             fragmentsTitle.add(title);
         }
@@ -139,6 +169,26 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, AboutActivity.class);
                 startActivity(intent);
                 return true;
+            case R.id.action_alarm: // Toggle Alarm
+                String toastMessage;
+                if (prefs.getBoolean("enabled", true)) { // Jika true, matikan
+                    disableNotification(null);
+                    toastMessage = "Alarm Off!";
+                    if (alarmManager != null) {
+                        alarmManager.cancel(notifyPendingIntent);
+                    }
+                } else {
+                    enableNotification(null);
+                    toastMessage = "Alarm On!";
+                    long repeatInterval = AlarmManager.INTERVAL_DAY; // 1 Day
+                    long triggerTime = SystemClock.elapsedRealtime() + repeatInterval;
+                    if (alarmManager != null) {
+                        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                triggerTime, repeatInterval, notifyPendingIntent);
+                    }
+                }
+                Toast.makeText(MainActivity.this, toastMessage,Toast.LENGTH_SHORT).show();
+                return true;
             default:
                 // Do nothing
         }
@@ -158,6 +208,47 @@ public class MainActivity extends AppCompatActivity {
                 if (info.isConnected()) have_MobileData = true;
         }
         return have_WIFI || have_MobileData;
+    }
+
+    public void createNotificationChannel() {
+        // Create a notification manager object.
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Reminder notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifies every 1 day");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    public void recordRunTime() {
+        editor.putLong("lastRun", System.currentTimeMillis());
+        editor.commit();
+    }
+
+    public void enableNotification(View v) {
+        editor.putLong("lastRun", System.currentTimeMillis());
+        editor.putBoolean("enabled", true);
+        editor.commit();
+        Log.v(TAG, "Notifications enabled");
+    }
+
+    public void disableNotification(View v) {
+        editor.putBoolean("enabled", false);
+        editor.commit();
+        Log.v(TAG, "Notifications disabled");
     }
 
 }
